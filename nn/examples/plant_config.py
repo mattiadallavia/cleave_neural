34997b96e360cd93b3b1d62d8c7c83cfa0a9a80f
Sara Vestergren <sara.vestergren@ericsson.com>
2020-11-01 16:47:48 +0100

    Get new PID to nn branch

---
+++ nn/examples/plant_config.py
@@ -0,0 +1,36 @@
+#  Copyright (c) 2020 KTH Royal Institute of Technology
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Example config file for an inverted pendulum plant
+# Example used to generate training data for Neural Network
+from cleave.base.client import SimpleActuator, SimpleSensor
+from cleave.base.network.client import UDPControllerInterface
+from cleave.impl import InvPendulumStateNoPyglet
+
+host = 'localhost'
+port = 50000
+
+state = InvPendulumStateNoPyglet(upd_freq_hz=200)
+controller_interface = UDPControllerInterface
+
+sensors = [
+    SimpleSensor('position', 100),
+    SimpleSensor('speed', 100),
+    SimpleSensor('angle', 100),
+    SimpleSensor('ang_vel', 100),
+]
+
+actuators = [
+    SimpleActuator('force')
+]
