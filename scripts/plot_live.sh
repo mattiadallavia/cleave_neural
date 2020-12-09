73bd243fffbe28c0de7afef5dcde1a2d113c2edd
Mattia Dalla Via <mattia@dallavia.it>
2020-12-09 17:05:56 +0100

    Renamed output parameter in live plot script

--- scripts/plot_live.sh
+++ scripts/plot_live.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-controller_name=$1 # name of the controller
+output_name=$1 # name of the controller
 datafile_name=$2 # name of the datafile
 plot_name=$3 # name of the plot
 
 while true
 do
 	gnuplot -e "set terminal pdf font 'Arial,12' size 12cm, 8cm; \
-				set output 'build/$controller_name.pdf'; \
+				set output 'build/$output_name.pdf'; \
 				datafile = 'build/$datafile_name.dat'; \
 				load 'plots/$plot_name.plt'; \
 				unset output"
